a43546d034a7dcb818bbf4471bcc83108e036371
Sayani Pramanik <92252926+AddoElektra@users.noreply.github.com>
2024-08-17 21:31:40 +0530

    Add files via upload

---
+++ tests/project1_page2.spec.js
@@ -0,0 +1,46 @@
+const { test, expect } = require('@playwright/test');
+
+test('title', async ({ page }) => {
+    await page.goto('https://www.amazon.in/');
+    const logo= page.locator('div[id="nav-logo"]');
+    await expect(logo).toBeVisible();
+  });
+
+  test('search', async ({ page }) => {
+    await page.goto('https://www.amazon.in/');
+    await page.locator('input[name="field-keywords"]').fill('iPhone15');
+    await page.locator('input[id="nav-search-submit-button"]').click();
+    })
+
+test('login', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.locator('span[class="nav-line-2 "]').click();
+  page.waitForLoadState('domcontentloaded');
+  const img= page.locator('h1[class="a-size-medium-plus a-spacing-small"]');
+  await expect(img).toBeVisible();
+  await page.locator('input[type="email"]').fill('sayani.pramanik004@gmail.com');
+  await page.locator('input[aria-labelledby="continue-announce"]').click();
+  //await expect(page).toHaveURL(/\/gp\/yourstore\/home/);
+});
+
+test('prime', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.locator('a[id="nav-link-amazonprime"]').click();
+  page.waitForLoadState('domcontentloaded');
+  const image=page.locator('img[alt="Prime logo"]');
+  await expect(image).toBeVisible();
+  const title=page.locator('h1[class="a-spacing-base a-spacing-top-medium"]').first();
+  await expect(title).toContainText('Welcome to Prime');
+  page.locator('span[class="a-button-inner"]').click();
+  page.waitForLoadState('networkidle');
+});
+
+test('filter', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.locator('a[data-csa-c-slot-id="nav_cs_5"]').click();
+  await page.waitForLoadState('domcontentloaded');
+  await page.locator('input[aria-labelledby="Prime Eligible"]').click();
+  await page.waitForLoadState('networkidle');
+  const title=page.locator('h2[class="a-size-medium-plus a-spacing-none a-color-base a-text-bold"]');
+  await expect(title).toContainText('Results');
+});
