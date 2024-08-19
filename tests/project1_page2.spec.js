4c299163676212b05b43e8c42ba278dceb9dc506
Sayani Pramanik <92252926+AddoElektra@users.noreply.github.com>
2024-08-19 11:05:26 +0530

    Add files via upload

---
+++ tests/project1_page2.spec.js
@@ -0,0 +1,59 @@
+const { test, expect } = require('@playwright/test');
+
+test('title', async ({ page }) => {
+    await page.goto('https://www.amazon.in/');
+    const logo= page.locator('div[id="nav-logo"]');
+    await expect(logo).toBeVisible();
+  });
+
+  test('search', async ({ page }) => {
+    await page.goto('https://www.amazon.in/');
+    await page.locator('input[name="field-keywords"]').fill('iPhone15');
+    await page.locator('input[id="nav-search-submit-button"]').click();
+    })
+
+test('login', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.locator('span[class="nav-line-2 "]').click();
+  page.waitForLoadState('domcontentloaded');
+  const img= page.locator('h1[class="a-size-medium-plus a-spacing-small"]');
+  await expect(img).toBeVisible();
+  await page.locator('input[type="email"]').fill('sayani.pramanik004@gmail.com');
+  await page.locator('input[aria-labelledby="continue-announce"]').click();
+  //await expect(page).toHaveURL(/\/gp\/yourstore\/home/);
+});
+
+test('prime', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.waitForLoadState('domcontentloaded');
+  await page.locator('a[id="nav-link-amazonprime"]').click();
+  await page.waitForLoadState('domcontentloaded');
+  const image = page.locator('img[alt="Prime logo"]');
+  await image.waitFor({ state: 'visible' });
+  await expect(image).toBeVisible();
+  const title = page.locator('h1[class="a-spacing-base a-spacing-top-medium"]').first();
+  await title.waitFor({ state: 'visible' });
+  await expect(title).toContainText('Welcome to Prime');
+  await page.locator('span[class="a-button-inner"]').click();
+  //await page.waitForLoadState('networkidle');
+});
+
+
+/*test('filter', async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto('https://www.amazon.in/');
+  await page.locator('a[data-csa-c-slot-id="nav_cs_5"]').click();
+  await page.waitForLoadState('load');
+  await page.locator('input[aria-labelledby="Prime Eligible"]').click();
+  //await page.waitForLoadState('networkidle');
+  //const title=page.locator('h2[class="a-size-medium-plus a-spacing-none a-color-base a-text-bold"]');
+  //await expect(title).toContainText('Results');
+});*/
+
+test('cart', async ({ page }) => {
+  //test.setTimeout(60000);
+  await page.goto('https://www.amazon.in/');
+  await page.locator('div[class=" nav-progressive-attribute"]').click();
+  //await page.waitForLoadState('networkidle');
+  await expect(page.locator('div[class="a-row sc-your-amazon-cart-is-empty"]')).toHaveText('Your Amazon Cart is empty');
+});
